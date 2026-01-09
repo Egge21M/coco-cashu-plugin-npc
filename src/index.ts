cf0691b3b532ec7c5dc4f2819c1c6004989f0b83
Egge <egge@my2sats.space>
2026-01-09 12:47:45 +0100

    fix: include type declaration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 export * from "./plugins/NPCPlugin";
 export * from "./sync/sinceStore";
 export * from "./types";
+export * from "./PluginApi";
+
+import type { PluginApi } from "./PluginApi";
+
+declare module "coco-cashu-core" {
+  interface PluginExtensions {
+    npc: PluginApi;
+  }
+}
